3862d31605cf9689a7c49f12a807b2bbb00f26b3
kk <lightrider1234@gmail.com>
2020-01-02 20:56:38 +0800

    fixed

--- service-worker.js
+++ service-worker.js
@@ -25,6 +25,37 @@ self.addEventListener('message', (event) => {
   }
 });
 
+// Listen to fetch events
+self.addEventListener('fetch', function(event) {
+
+	// Clone the request
+	var req = event.request.clone();
+
+  	// Check if the image is a jpeg
+  	if (/\.jpg$|.png$/.test(event.request.url)) {
+
+  		// Get all of the headers
+  		let headers = Array.from(req.headers.entries());
+
+  		// Inspect the accept header for WebP support
+  		var acceptHeader = headers.find(item => item[0] == 'accept');
+  		var supportsWebp = acceptHeader[1].includes('webp');
+
+  		// If we support WebP
+  		if (supportsWebp)
+  		{
+        // Build the return URL
+  			var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
+
+          event.respondWith(
+          fetch(returnUrl, {
+            mode: 'no-cors'
+          })
+        );
+  	}
+	}
+});
+
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
